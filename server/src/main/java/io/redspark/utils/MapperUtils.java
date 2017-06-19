39b8c58ae91354ca5dc882604fe0908f471cfff1
Leandro Lacau <llacau@gmail.com>
2017-06-19 12:56:39 -0300

    Remove some unused methods.

--- server/src/main/java/io/redspark/utils/MapperUtils.java
+++ server/src/main/java/io/redspark/utils/MapperUtils.java
@@ -4,8 +4,6 @@ import io.redspark.utils.mapper.IgnoreLazyCodeGenerationStrategy;
 import ma.glasnost.orika.MapperFacade;
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
-import ma.glasnost.orika.metadata.ClassMapBuilder;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -26,7 +24,9 @@ public class MapperUtils<E, D> {
         this.dtoClass = dtoClass;
     }
 
-    @Transactional(readOnly = true)
+    // @Transactional Removed! it will have no effect if not called through a proxy
+    // Class should be managed by spring
+    //    @Transactional(readOnly = true)
     public D toDTO(E entity) {
         return mapper.map(entity, dtoClass);
     }
@@ -34,43 +34,4 @@ public class MapperUtils<E, D> {
     public List<D> toDTO(List<E> entities) {
         return mapper.mapAsList(entities, dtoClass);
     }
-
-    public D toDTO(E entity, String... exclusions) {
-        MapperFactory instanceFactory = createFactory();
-        ClassMapBuilder<D, E> classMap = instanceFactory.classMap(dtoClass, entityClass);
-        for (String exc : exclusions) {
-            classMap.exclude(exc);
-        }
-        classMap.byDefault().register();
-        return instanceFactory.getMapperFacade().map(entity, dtoClass);
-    }
-
-    public E toEntity(D dto) {
-        return mapper.map(dto, entityClass);
-    }
-
-    public E toEntity(D dto, String... exclusions) {
-        MapperFactory instanceFactory = createFactory();
-        ClassMapBuilder<E, D> classMap = instanceFactory.classMap(entityClass, dtoClass);
-        for (String exc : exclusions) {
-            classMap.exclude(exc);
-        }
-        classMap.byDefault().register();
-        return instanceFactory.getMapperFacade().map(dto, entityClass);
-    }
-
-    /**
-     * Thread-safe. Create a new MapperFacade for every update
-     */
-    public void updateEntity(E entity, D dto, String... exclusions) {
-        MapperFactory instanceFactory = createFactory();
-        ClassMapBuilder<E, D> classMap = instanceFactory.classMap(entityClass, dtoClass);
-        for (String exc : exclusions) {
-            classMap.exclude(exc);
-        }
-        classMap.byDefault().register();
-
-        instanceFactory.getMapperFacade(entityClass, dtoClass).mapReverse(dto, entity);
-    }
-
 }
